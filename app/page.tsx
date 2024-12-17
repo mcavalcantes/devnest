3af6e3c4516879402f6874ce6c7873a2e89d20cc
Matheus Cavalcante <matheuscavalcantes.mc@gmail.com>
2024-12-17 14:38:51 -0300

    installed shadcn and made the mvp home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function App() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold">hello world</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="h-20 text-4xl font-bold flex items-center">DevNest</h1>
+      <div className="flex flex-col md:flex-row gap-4">
+        <Link href="/login" className={buttonVariants({ variant: "default" })}>Login</Link>
+        <Link href="/signup" className={buttonVariants({ variant: "outline" })}>Cadastrar</Link>
+      </div>
     </main>
   );
 }
